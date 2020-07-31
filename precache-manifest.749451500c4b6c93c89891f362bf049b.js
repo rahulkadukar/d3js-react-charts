bdbdb7870840bd3168fb879a2e393509cd94652e
Rahul Kadukar <rahulkadukar@yahoo.com>
2020-07-30 20:28:05 -0400

    Updates

---
+++ precache-manifest.749451500c4b6c93c89891f362bf049b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65541c622ec45fdf5fc177a08bd71a9a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dbe12dfc9af81b86b9f3",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "cf5545af15b3afe4e3fc",
+    "url": "./static/js/2.f18a5b0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f18a5b0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe12dfc9af81b86b9f3",
+    "url": "./static/js/main.2ac81de2.chunk.js"
+  },
+  {
+    "revision": "0f761efbdad9d706903a",
+    "url": "./static/js/runtime-main.1d51c54c.js"
+  }
+]);
